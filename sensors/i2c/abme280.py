e7b638868e2950e423727f93bb0803b25f32ab41
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-11-05 09:55:20 -0700

    going to start messing up the video pipeline

--- sensors/i2c/abme280.py
+++ sensors/i2c/abme280.py
@@ -2,7 +2,7 @@ from adafruit_bme280 import basic as adafruit_bme280
 import sys
 repoPath = "/home/pi/Documents/"
 sys.path.append(repoPath + "unifiedSensorClient/")
-from sensors.i2c.i2cSensor import Sensor
+from sensors.sensor import Sensor
 import logging
 
 # open the config
@@ -11,22 +11,22 @@ config = i2c_controller_process_config
 # find the config for this device
 device_config = [d for d in i2c_controller_process_config['devices'] if d['module_name'] == 'abme280'][0]
 
-class aBME280:
+class aBME280():
     def __init__(self, bus):
         self.l = logging.getLogger(config["short_name"] + "." + device_config['model'])
         self.l.setLevel(device_config['debug_lvl'])
-        self.l.info(config["short_name"] + " starting a " + device_config['model'] + "!")
+        self.l.info("i2c starting a " + device_config['model'] + "!")
         self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(bus, address=device_config['address'])
         
         self.is_ready = lambda: True
 
-        self.get_temp_c = lambda: self.bme280.temperature
+        self.get_air_temperature = lambda: self.bme280.temperature
         self.get_relative_humidity = lambda: self.bme280.relative_humidity
-        self.get_pressure_pa = lambda: self.bme280.pressure * 100
+        self.get_barometric_pressure = lambda: self.bme280.pressure * 100
         
-        retrieve_datas = {'air-temprature-celcius': self.get_temp_c,
-                            'relative-humidity-percent': self.get_relative_humidity,
-                            'barometric-pressure-pa': self.get_pressure_pa}
+        retrieve_datas = {'air-temperature': self.get_air_temperature,
+                            'relative-humidity': self.get_relative_humidity,
+                            'barometric-pressure': self.get_barometric_pressure}
 
 
         self.sensors = []
