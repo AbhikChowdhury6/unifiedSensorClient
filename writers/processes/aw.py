946194f59ced41e3273153fa290a6dd464401d83
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-11-06 11:02:41 -0700

    I think the writer is looking good

--- writers/processes/aw.py
+++ writers/processes/aw.py
@@ -14,8 +14,8 @@ import zmq
 import logging
 import numpy as np
 from platformUtils.logUtils import worker_configurer, set_process_title
-import shutil
 from writers.writer import Writer
+import pickle
 
 config = audio_writer_process_config
 l = logging.getLogger(config["short_name"])
@@ -30,29 +30,34 @@ def audio_writer(log_queue):
     sub.setsockopt(zmq.SUBSCRIBE, config["sub_topic"].encode())
     l.info(config["short_name"] + " writer subscribed to " + config["sub_topic"])
 
-    persist = lambda x, y: pickle.dump(y, open(x, "wb"))
-    persist_open = lambda x: pickle.load(open(x, "rb"))
+    def persist(dt, data):
+        path = config["cache_location"] + dt_to_fnString(dt, 6) + ".pkl"
+        pickle.dump(data, open(path, "wb"))
 
-    class output:
-        def __init__(self, file_name):
-            self.file_name = file_name
-            self.file = open(file_name, "wb")
-        def write(self, data):
-            self.file.write(data)
-        def close(self):
-            self.file.close()
+    def load(path):
+        return fnString_to_dt(path), pickle.load(open(path, "rb"))
 
-
-    writer = Writer(config["writer_config"])
+    writer = Writer(config["writer_config"], persist, load)
     while True:
         topic, msg = ZmqCodec.decode(sub.recv_multipart())
 
-#takes in a dt and returns a file object
+        if topic == "control" and (msg[0] == "exit_all" or 
+                (msg[0] == "exit" and msg[-1] == config["short_name"])):
+            l.info(config["short_name"] + " writer exiting")
+            break
+
+        if topic != config["sub_topic"]:
+            continue
+
+        dt, chunk = msg
+        writer.write(dt, chunk)
+
 
 class output:
     def __init__(self):
         self.ff = None
-        self.is_open = False
+        self.file_name = None
+        self.temp_output_root = None
     
     def open(self, dt: datetime):
         """Spawn ffmpeg to encode PCM from stdin into Opus segments using config.
@@ -68,9 +73,9 @@ class output:
         loglevel = str(config["loglevel"])
         sample_fmt = "s16le"
         file_base = config["file_base"]
-        temp_file_name = file_base + "_" + dt_to_fnString(dt, 6) + ".opus"
+        self.file_name = file_base + "_" + dt_to_fnString(dt, 6) + ".opus"
 
-        file_name = output_root + temp_file_name
+        file_name_and_path = output_root + self.file_name
 
         cmd = [
             "ffmpeg",
@@ -83,7 +88,7 @@ class output:
             "-c:a", "libopus",
             "-b:a", bitrate,
             "-frame_duration", str(frame_duration_ms),
-            file_name,
+            file_name_and_path,
         ]
 
         #standard error reader and running ffmpeg
@@ -98,11 +103,12 @@ class output:
             l.debug(config["short_name"] + " writer: started ffmpeg: " + " ".join(cmd))
 
             # Start a background stderr reader for diagnostics
-            t = threading.Thread(target=_stderr_reader, args=(proc,), daemon=True)
+            t = threading.Thread(target=self._stderr_reader, args=(proc,), daemon=True)
             t.start()
             proc._stderr_thread = t  # attach for lifecycle awareness
             self.ff = proc
             self.is_open = True
+            return self.file_name
         except FileNotFoundError:
             l.error(config["short_name"] + " writer: ffmpeg not found. Please install ffmpeg.")
             return None
@@ -111,7 +117,7 @@ class output:
             return None
 
 
-    def _stderr_reader(p):
+    def _stderr_reader(self, p):
         try:
             for raw in iter(p.stderr.readline, b""):
                 line = raw.decode(errors="replace").rstrip()
@@ -122,7 +128,7 @@ class output:
         finally:
             l.debug(config["short_name"] + " writer: ffmpeg stderr: [closed]")
     
-    def close(self):
+    def close(self, dt: datetime):
         if self.ff.stdin is not None:
             self.ff.stdin.close()
         
@@ -137,6 +143,13 @@ class output:
         except Exception as e:
             l.error(config["short_name"] + " writer: failed to kill ffmpeg: " + str(e))
         self.ff = None
+
+        new_fn = self.file_name.replace("." + self.extension, 
+                                            "_" + dt_to_fnString(dt, 6) + "." + self.extension)
+        os.rename(self.temp_output_root + self.file_name, 
+                  self.temp_output_root + new_fn)
+        self.file_name = None
+        return new_fn
 
     def write(self, data):
         self.ff.stdin.write(data)
--- writers/writer.py
+++ writers/writer.py
@@ -12,7 +12,7 @@ import numpy as np
 from platformUtils.logUtils import worker_configurer, set_process_title
 import shutil
 from datetime import datetime, timezone, timedelta
-
+import random
 
 class output:
     def __init__(self, file_name):
@@ -27,7 +27,8 @@ class output:
 
 
 class Writer:
-    def __init__(self, config, output, perisist, open_function):
+    def __init__(self, config, output, persist, load_function):
+        self.config = config
         self.l = logging.getLogger(config["short_name"])
         self.l.setLevel(config['debug_lvl'])
         self.l.info(config["short_name"] + " writer starting")
@@ -41,62 +42,93 @@ class Writer:
 
         self.output_base = config["output_base"]
         self.extension = config["extension"]
+        self.persist = persist
+        self.load = load_function
         
         self.output = output
         self.output_file = ""
         self.output_start_dt = None
 
         #deciding to close
-        self.previous_dt = None
+        self.last_dt = None
         self.target_file_size_mb = config["target_file_size_mb"]
+        self.next_size_check_dt = datetime.min.replace(tzinfo=timezone.utc)
+        self.size_check_interval_s_range = (30, 60)
         self.expected_hz = config["expected_hz"]
 
+        #if the last move or anything else failed, delete all the temp files
+        for file in os.listdir(self.temp_file_location).sorted():
+            os.remove(self.temp_file_location + file)
+        
         #check for files in cache and recover
-
+        self._recover_from_cache()
 
     def _recover_from_cache(self):
         files = os.listdir(self.cache_location).sorted()
         if len(files) == 0:
             return
-        self.l.info(config["short_name"] + " writer found " + str(len(files)) + " files in cache")
+        self.l.info(self.config["short_name"] + " writer found " + str(len(files)) + " files in cache")
         
         for file in files:
-            self.write(fnString_to_dt(file), open_function(file))
+            dt, data = self.load(self.cache_location + file)
+            self.write(dt, data)
 
     def _open_file(self, dt): 
         self.output_start_dt = dt
-        self.output_file = self.temp_file_location + self.output_base + "_" + dt_to_fnString(dt) + "." + self.extension        
-        self.output.open(dt)
+       
+        self.output_file = self.output.open(dt)
 
     def _close_file(self, dt):
-        self.output.close()
-        #rename the file with the end timestamp to signal it is complete
+        self.output_file = self.output.close(dt)
+
         #move the file to the correct location in data
-        #delete the cached files older than the last dt
-        #delete all the cached files older than the last dt
-        pass
+        finished_file_name = self.output_base + self.output_file
+        infile = self.temp_file_location + self.output_file
+        outfile = self.completed_file_location + finished_file_name
+        shutil.move(infile, outfile)
+        self.output_file = None
+        self.last_dt = None
+        
+        #delete all the cached files , they should all be older than the last dt
+        for file in os.listdir(self.cache_location).sorted():
+            os.remove(self.cache_location + file)
 
     def _should_close(self, dt):
+        if self.output.file_name is None:
+            return False
+        #new day
         if dt.date() != self.last_dt.date():
             return True
-        if dt - self.last_dt > timedelta(seconds=1/self.expected_hz):
+        #too long since last write
+        if dt - self.last_dt > 2 * timedelta(seconds=1/self.expected_hz):
             return True
 
-        #figure out how to get this from the files system
-        output_size = None
-        if self.output.size > self.target_file_size_mb * 1024 * 1024:
+        # Get the current size on disk of the output file
+        if dt < self.next_size_check_dt:
+            return False
+        
+        rand_s = random.randint(*self.size_check_interval_s_range)
+        self.next_size_check_dt = dt + timedelta(seconds=rand_s)
+        
+        out_file_and_path = self.temp_file_location + self.output_file
+        output_size = os.path.getsize(out_file_and_path)
+        if output_size > self.target_file_size_mb * 1024 * 1024:
             return True
+        
         return False
 
     def write(self, dt, data):
         if self._should_close(dt):
             self._close_file(dt)
 
-        if self.output.is_open is False:
+        self.persist(dt, data)
+        
+        if self.output.file_name is None:
             self.output.open(dt)
         
         self.output.write(data)
-        
+
+        self.last_dt = dt
         
 
 
