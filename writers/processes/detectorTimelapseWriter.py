4647eafa3d345738e3492d7061f04ad2ab81cde4
AbhikChowdhury6 <abhik.ch6@gmail.com>
2026-01-15 18:23:49 -0700

    wasn't adding the right time delta

--- writers/processes/detectorTimelapseWriter.py
+++ writers/processes/detectorTimelapseWriter.py
@@ -250,7 +250,7 @@ def detector_timelapse_writer(log_queue, config):
         delete_old_files(dt_utc)
 
         for i in range(int(1/timelapse_hz)):
-            frame_dt = dt_utc - timelapse_frame_offset + timedelta(seconds=i/timelapse_hz)
+            frame_dt = dt_utc - timelapse_frame_offset + timedelta(seconds=i)
             l.trace(" dbtl writing timelapse frame at " + str(frame_dt))
             timelapse_writer.write(frame_dt, curr_timelapse_frame)
         next_timelapse_frame_update += timedelta(seconds=1/timelapse_hz)
